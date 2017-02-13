ccfdce39de85201d0ff5d688f5518c51861e1fbe
tanjianhui <tan_jianhui@163.com>
2017-02-13 09:17:09 +0800

    Initial commit

---
+++ src/main/java/com/springbootdemo/service/ScheduleTaskMonitorService.java
@@ -0,0 +1,33 @@
+package com.springbootdemo.service;
+
+import com.springbootdemo.dao.ScheduleTaskMonitorMapper;
+import com.springbootdemo.model.ScheduleTaskMonitor;
+import com.springbootdemo.model.ScheduleTaskMonitorExample;
+import com.springbootdemo.sdk.service.BaseService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by tanjianhui on 2017/2/13.
+ */
+@Service("scheduleTaskMonitorService")
+public class ScheduleTaskMonitorService extends BaseService<Integer, ScheduleTaskMonitor, ScheduleTaskMonitorExample,
+        ScheduleTaskMonitorMapper> {
+    private ScheduleTaskMonitorMapper scheduleTaskMonitorMapper;
+
+    @Override
+    protected ScheduleTaskMonitorMapper getMapper(){
+        return this.scheduleTaskMonitorMapper;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public int addRequestNew(ScheduleTaskMonitor record){
+        return this.scheduleTaskMonitorMapper.insertSelective(record);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public int updateRequestNew(ScheduleTaskMonitor record){
+        return this.scheduleTaskMonitorMapper.updateByPrimaryKeySelective(record);
+    }
+}
