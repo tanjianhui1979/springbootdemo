4720b2d0f4729cf470e2da735cfc64c72f9909b0
tanjianhui <tan_jianhui@163.com>
2017-02-15 10:20:26 +0800

    新增insertSelectiveUseGeneratedKeys方法

--- src/main/java/com/springbootdemo/dao/ScheduleTaskMonitorMapper.java
+++ src/main/java/com/springbootdemo/dao/ScheduleTaskMonitorMapper.java
@@ -8,6 +8,9 @@ import com.springbootdemo.sdk.dao.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 
 public interface ScheduleTaskMonitorMapper extends BaseMapper<Integer, ScheduleTaskMonitor, ScheduleTaskMonitorExample> {
+
+    int insertSelectiveUseGeneratedKeys(ScheduleTaskMonitor record);
+
     List<ScheduleTaskMonitor> selectByExampleWithBLOBs(ScheduleTaskMonitorExample example);
 
     int updateByExampleWithBLOBs(@Param("record") ScheduleTaskMonitor record, @Param("example") ScheduleTaskMonitorExample example);
