836deb04c55fc7731110b43ec80e35ddfb2de6d5
tanjianhui <tan_jianhui@163.com>
2017-02-15 08:25:20 +0800

    Initial Commit

---
+++ src/main/java/com/springbootdemo/sdk/model/BaseModel.java
@@ -0,0 +1,9 @@
+package com.springbootdemo.sdk.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by tanjianhui on 2017/2/13.
+ */
+public abstract class BaseModel implements Serializable{
+}
