e6fa1ff753d11ef9c1bc266806f05e2a4ca92b21
tanjianhui <tan_jianhui@163.com>
2017-02-15 10:23:47 +0800

    简化代码

--- src/main/java/com/springbootdemo/mq/producer/SampleProducer.java
+++ src/main/java/com/springbootdemo/mq/producer/SampleProducer.java
@@ -1,6 +1,9 @@
 package com.springbootdemo.mq.producer;
 
 import com.rabbitmq.client.AMQP;
+import com.springbootdemo.model.ScheduleTaskMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +14,16 @@ import org.springframework.stereotype.Component;
  */
 @Component("sampleProducer")
 public class SampleProducer {
+    private static final Logger logger = LoggerFactory.getLogger(SampleProducer.class);
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    private Queue rabbitQueue;
-
     public void send(){
         rabbitTemplate.convertAndSend("Hello, world!");
+    }
+
+    public void send(ScheduleTaskMonitor record){
+        rabbitTemplate.convertAndSend(record);
     }
 }
